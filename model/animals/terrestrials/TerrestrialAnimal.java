7738653139ff6fe588e2de5f67b56cd952741839
Clément_Driesch <151400068+UnfairB@users.noreply.github.com>
2026-09-15 12:01:23 -0400

    le z animos terrest

---
+++ model/animals/terrestrials/TerrestrialAnimal.java
@@ -0,0 +1,13 @@
+package model.animals.terrestrials;
+
+import model.animals.Animal;
+
+import java.util.Date;
+
+public abstract class TerrestrialAnimal extends Animal {
+    private String subType;
+    TerrestrialAnimal(String name, String adress, Date arrivalDate, boolean adopted, int age, String bloodType, String subType){
+        super(name,adress,arrivalDate,adopted,age,bloodType,"Terrestrial");
+        this.subType = subType;
+    }
+}
