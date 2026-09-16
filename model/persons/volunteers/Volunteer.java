19e9849f4dc67fa541b160b6e86c7dbddcf47f9c
Clément_Driesch <151400068+UnfairB@users.noreply.github.com>
2026-09-16 12:01:59 -0400

    Voluntears );

--- model/persons/volunteers/Volunteer.java
+++ model/persons/volunteers/Volunteer.java
@@ -2,15 +2,37 @@ package model.persons.volunteers;
 
 import model.persons.Person;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class Volunteer extends Person {
     private boolean availability;
-    private String skills;
+    private Set<String> skills = new HashSet<>();
     private boolean assignmentStatus;
 
-    public Volunteer(String id, int age, String adress, boolean availability, String skills, boolean assignmentStatus){
-        super(id,age,adress);
+    public Volunteer(String id, int age, String adress, boolean availability, boolean assignmentStatus) {
+        super(id, age, adress);
         this.availability = availability;
-        this.skills = skills;
         this.assignmentStatus = assignmentStatus;
     }
+
+    public void addSkill(String skill) {
+        skills.add(skill);
+    }
+
+    public boolean containSkill(String skill) {
+        return skills.contains(skill);
+    }
+
+    public void displaySkill() {
+        Iterator<String> iterator = skills.iterator();
+        int count = 1;
+        while (iterator.hasNext()) {
+            String skill = iterator.next();
+            System.out.println("Skill number " + count + " : " + skill);
+            count++;
+        }
+    }
 }
+
